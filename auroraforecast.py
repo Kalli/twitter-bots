7143ceb7460fbb7dbe1945c169102f22097e2a14
Karl Tryggvason <ktryggvason@gmail.com>
2013-08-10 19:24:56 +0000

    Branch for itaurorahunt

--- auroraforecast.py
+++ auroraforecast.py
@@ -6,7 +6,6 @@ import datetime
 import tweepy
 import credentials
 
-
 def tweetauroraforecast():
     url = "http://en.vedur.is/weather/forecasts/aurora/"
     r = requests.get(url)
@@ -16,22 +15,17 @@ def tweetauroraforecast():
     datestring = now.strftime("%y%m%d")
     timestring = now.strftime("%H:00 - %d.%m.%y")
 
-    # Forecasts come at three hour intervals from midnight find the closest one
-    forecasttimes = range(3, 25, 3)
-    closestforecast = forecasttimes[min(range(len(forecasttimes)), key=lambda i: abs(forecasttimes[i]-now.hour))]
-
     # Get the cloud cover image
-    imgstring = "http://en.vedur.is/photos/ecm0125_island_tcc/"+now.strftime("%y%m%d_0000_")+str(closestforecast)+".png"
+    description={"0":"Minimum","1":"Quiet","2":"Low","3":"Moderate","4":"Active","5":"High","6":"Very high","7":"Strong","8":"Severe","9":"Extreme"}
 
     # Find the index javascript
     soup = BeautifulSoup(r.text.encode('utf-8'))
     indexes = soup.findAll("script", text=re.compile("VI.data.aurora.idx"))
     index_re = re.compile(datestring + ".*act:\s(\d)")
     index = index_re.findall("".join(indexes))
-
     tweettext = ""
     if len(index) == 1:
-        tweettext = "Aurora activity in Iceland: %s/9\nDetails: %s\nCloud cover: %s\n%s" % (index[0], url, imgstring, timestring)
+        tweettext = "Current aurora activity in Iceland: %s - %s/9\nDetails: %s\n%s" % (description[index[0]], index[0], url,  timestring)
     if len(tweettext) > 0:
         auth = tweepy.OAuthHandler(credentials.AURORA_CONSUMER_KEY, credentials.AURORA_CONSUMER_SECRET)
         auth.set_access_token(credentials.AURORA_ACCESS_TOKEN, credentials.AURORA_ACCESS_SECRET)
