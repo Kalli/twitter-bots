81c5d742c04b029c0f938df3e2d36044ff58e5b1
Karl Tryggvason <ktryggvason@gmail.com>
2013-09-25 22:02:18 +0100

    New branch for different Aurora Bot

--- auroraforecast.py
+++ auroraforecast.py
@@ -4,34 +4,41 @@ from BeautifulSoup import BeautifulSoup
 import re
 import datetime
 import tweepy
-import credentials
+import metadata
+import random
 
-def tweetauroraforecast():
+
+def get_aurora_description(descriptions, rating):
+    return random.choice(descriptions[rating])
+
+
+def get_aurora_activity_rating():
     url = "http://en.vedur.is/weather/forecasts/aurora/"
     r = requests.get(url)
     r.encoding
 
     now = datetime.datetime.now()
     datestring = now.strftime("%y%m%d")
-    timestring = now.strftime("%H:00 - %d.%m.%y")
-
-    # Get the cloud cover image
-    description={"0":"Minimum","1":"Quiet","2":"Low","3":"Moderate","4":"Active","5":"High","6":"Very high","7":"Strong","8":"Severe","9":"Extreme"}
 
     # Find the index javascript
     soup = BeautifulSoup(r.text.encode('utf-8'))
     indexes = soup.findAll("script", text=re.compile("VI.data.aurora.idx"))
     index_re = re.compile(datestring + ".*act:\s(\d)")
     index = index_re.findall("".join(indexes))
+    return index
+
+
+def tweetauroraforecast():
+    index = get_aurora_activity_rating()
     tweettext = ""
     if len(index) == 1:
-        tweettext = "Current aurora activity in Iceland: %s - %s/9\nDetails: %s\n%s" % (description[index[0]], index[0], url,  timestring)
+        tweettext = get_aurora_description(metadata.descriptions, index[0]) + " http://en.vedur.is/weather/forecasts/aurora/"
     if len(tweettext) > 0:
-        auth = tweepy.OAuthHandler(credentials.AURORA_CONSUMER_KEY, credentials.AURORA_CONSUMER_SECRET)
-        auth.set_access_token(credentials.AURORA_ACCESS_TOKEN, credentials.AURORA_ACCESS_SECRET)
+        auth = tweepy.OAuthHandler(metadata.AURORA_CONSUMER_KEY, metadata.AURORA_CONSUMER_SECRET)
+        auth.set_access_token(metadata.AURORA_ACCESS_TOKEN, metadata.AURORA_ACCESS_SECRET)
         api = tweepy.API(auth)
         api.update_status(tweettext)
 
 tweetauroraforecast()
 import cloud
-cloud.cron.register(tweetauroraforecast, 'auroraforecast_tweetauroraforecast', '0 1,7,19,22 * * *', _type='s1', _max_runtime=10)
+cloud.cron.register(tweetauroraforecast, 'auroraforecast_tweetauroraforecast', '0 7,19,22 * * *', _type='s1', _max_runtime=10)
