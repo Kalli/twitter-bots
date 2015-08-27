03ec9ad13e5142bf0d26dd941a794dbeced59775
Karl Tryggvason <ktryggvason@gmail.com>
2015-08-27 22:24:32 +0200

    Updating Aurorabot

--- auroraforecast.py
+++ auroraforecast.py
@@ -2,9 +2,14 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 import re
-import datetime
+import time
 import tweepy
-import credentials
+import os
+
+CONSUMER_KEY = os.environ.get('AURORA_BOT_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('AURORA_BOT_CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('AURORA_BOT_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('AURORA_BOT_ACCESS_TOKEN_SECRET')
 
 
 def tweetauroraforecast():
@@ -12,16 +17,16 @@ def tweetauroraforecast():
     r = requests.get(url)
     r.encoding
 
-    now = datetime.datetime.now()
-    datestring = now.strftime("%y%m%d")
-    timestring = now.strftime("%H:00 - %d.%m.%y")
+    now = time.gmtime()
+    datestring = time.strftime("%y%m%d", now)
+    timestring = time.strftime("%H:00 - %d.%m.%y", now)
 
     # Forecasts come at three hour intervals from midnight find the closest one
     forecasttimes = range(3, 25, 3)
-    closestforecast = forecasttimes[min(range(len(forecasttimes)), key=lambda i: abs(forecasttimes[i]-now.hour))]
+    closestforecast = forecasttimes[min(range(len(forecasttimes)), key=lambda i: abs(forecasttimes[i]-now.tm_hour))]
 
     # Get the cloud cover image
-    imgstring = "http://en.vedur.is/photos/ecm0125_island_tcc/"+now.strftime("%y%m%d_0000_")+str(closestforecast)+".png"
+    imgstring = "http://en.vedur.is/photos/ecm0125_island_tcc/"+time.strftime("%y%m%d_0000_", now)+str(closestforecast)+".png"
 
     # Find the index javascript
     soup = BeautifulSoup(r.text.encode('utf-8'))
@@ -33,11 +38,17 @@ def tweetauroraforecast():
     if len(index) == 1:
         tweettext = "Aurora activity in Iceland: %s/9\nDetails: %s\nCloud cover: %s\n%s" % (index[0], url, imgstring, timestring)
     if len(tweettext) > 0:
-        auth = tweepy.OAuthHandler(credentials.AURORA_CONSUMER_KEY, credentials.AURORA_CONSUMER_SECRET)
-        auth.set_access_token(credentials.AURORA_ACCESS_TOKEN, credentials.AURORA_ACCESS_SECRET)
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         api = tweepy.API(auth)
         api.update_status(tweettext)
 
 tweetauroraforecast()
-import cloud
-cloud.cron.register(tweetauroraforecast, 'auroraforecast_tweetauroraforecast', '0 1,7,19,22 * * *', _type='s1', _max_runtime=10)
+
+while True:
+    time.sleep(60* 60)
+    if time.gmtime().tm_hour in [0, 6, 12, 18]:
+        try:
+            tweetauroraforecast()
+        except Exception:
+            print(traceback.format_exc())
